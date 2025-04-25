26f18b87f888d804b77ba6eb4c7cb654d0c95c63
Dain Kang <dainkang@Mac.nyc.rr.com>
2025-04-25 18:16:49 -0400

    add sandboxPersonas method for testing names

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const applicant = {
 }
 
 const personas = [
-    {...applicant, name_last: "Smith"},
+    {...applicant, name_last: "Kang"},
     {...applicant, name_last: "Review"},
     {...applicant, name_last: "Deny"}
 ]
@@ -40,17 +40,31 @@ async function postEval(person) {
             
             }
         );
-        console.log(resp.data, 'data')
+    console.log(resp.data, 'data')
 
     const result = resp.data?.summary?.outcome;
     console.log(result, 'result')
 
+    if (result == "Approved") { 
+        console.log("Congratulations! You are approved");
+    } else if (result == "Manual Review") {
+        console.log("Your application is under review. Please wait for further updates")
+    } else if (result == "Denied") { //the outcome is Denied for "Deny" name
+        console.log("Unfortunately, we cannot approve your application at this time.")
+    } else {
+        console.log("unknown error", resp.data, "outcome", result)
     }
-
+    }
     catch (err) {
     console.log(err, 'err')
     }
 }
 
+async function sandboxPersonas() {
+    console.log(personas,'personas') 
+    for(const person of personas) { //iterate through personas with different last names to coerce a different response.
+        await postEval(person) //each persona goes through with an evaulation
+    }
+}
 
-postEval(applicant)
+sandboxPersonas();
